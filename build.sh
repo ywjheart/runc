fb4c07fe570ce153a0e58d3c8dca8bc8d34aa068
Wenjiang Ye <yew@seagroup.com>
2019-03-26 14:22:16 +0800

    1. tweak build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 docker build -t runc_build .
-docker run -it  -v `pwd`:/go/src/github.com/opencontainers/runc runc_build make BUILDTAGS='seccomp apparmor'
+docker run -it  -v `pwd`:/go/src/github.com/opencontainers/runc runc_build make BUILDTAGS='seccomp apparmor selinux'
